cd579dcddff56e29b628106d68e181a51d74ef1f
donlapidos <donlapidos@gmail.com>
2022-11-01 18:26:28 +0800

    Awesome THREEJS Site

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        name: "house",
+        type: "glbmModel",
+        path: "/models/Finale Version 16.glb"
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/kda.mp4"
+    },
+
+]
